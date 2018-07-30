1d324ebe88769583e58199d8a4f0472723db7177
Ice-Media <41881731+Ice-Media@users.noreply.github.com>
2018-07-29 20:05:49 -0600

    Add files via upload

---
+++ Commands/bot info.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) =>{
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#0087ff")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Version", "0.5.6")
+    .addField("Creator", "Cynical's Dev Team")
+    .addField("Help?", "Ice#8526")
+
+    return message.channel.send(botembed);
+
+}
+
+module.exports.help = {
+    name: "botinfo"
+}
