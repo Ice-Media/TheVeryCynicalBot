e16a3eff5d2075df631f7b55dde46631f65abbeb
Corbin McLeod <41881731+Ice-Media@users.noreply.github.com>
2018-07-29 21:43:05 -0600

    Add files via upload

---
+++ Commands/admhelp.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    // if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Missing Permission: Manage Messages");
+
+    let adembed = new Discord.RichEmbed()
+    .setDescription("Admin Help")
+    .setColor("#00ff7b")
+    .addField("!kick", "Use command and tag someone with a reason following. Must have Message Managing Permissions to use.")
+    .addField("!ban", "Use command and tag someone with a reason following. Must have Role Managing Permissions to use.")
+    .addField("!say", "Outputs what you put after the command.")
+    .addField("!clear", "Put command and then a number after it. Will delete that many messages. Minimum 2.");
+
+    return message.channel.send(adembed);
+
+}
+
+module.exports.help = {
+    name: "admhelp"
+}
