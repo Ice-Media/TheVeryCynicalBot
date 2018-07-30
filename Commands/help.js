b39674bfd1332eb00946cb99ac3bd43f4e296941
Corbin McLeod <41881731+Ice-Media@users.noreply.github.com>
2018-07-29 20:45:02 -0600

    Add files via upload

---
+++ Commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let helpembed = new Discord.RichEmbed()
+.setDescription("Help")
+.setColor("#00ff7b")
+.addField("!botinfo", "Displays information about this bot.")
+.addField("!serverinfo", "Displays information about the server.")
+.addField("More Commands Soon to Come", "Be Patient");
+
+return message.channel.send(helpembed);
+
+}
+
+module.exports.help = {
+    name: "help"
+}
