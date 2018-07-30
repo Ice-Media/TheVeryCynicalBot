00d5a474fe171337efc1b40054d2344c0fe93080
MeaninglessVoid <dubnersamuel@gmail.com>
2018-07-30 01:17:35 -0700

    fix addrole and removerole

--- Commands/addrole.js
+++ Commands/addrole.js
@@ -1,21 +1,22 @@
 const Discord = require("Discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  let rMember =
+    message.guild.member(message.mentions.users.first()) ||
+    message.guild.members.get(args[0]);
+  if (!rMember) return message.reply("Could not find User.");
+  let role = message.content.split(" ").splice(2).join(" ");
+  if (!role) return message.reply("specify a Role.");
+  let gRole = message.guild.roles.find(`name`, role);
+  if (!gRole) return message.reply("Couldn't find that Role.");
 
-let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-if(!rMember) return message.reply("Could not find User.");
-let role = args.join(" ").slice(22);
-if(!role) return message.reply("specify a Role.");
-let gRole = message.guild.roles.find(`name`, role);
-if(!gRole) return message.reply("Couldn't find that Role.");
+  if (rMember.roles.has(gRole.id))
+    return message.reply("They already have that role.");
+  await rMember.addRole(gRole.id);
 
-if(rMember.roles.has(gRole.id))return message.reply("They already have that role.");
-await(rMember.addRole(gRole.id));
-
-message.channel.send(`<@${rMember.id}> You now have the ${gRole.name} role.`)
-
-}
+  message.channel.send(`<@${rMember.id}> You now have the ${gRole.name} role.`);
+};
 
 module.exports.help = {
-    name: "addrole"
-}
+  name: "addrole"
+};
--- Commands/removerole.js
+++ Commands/removerole.js
@@ -1,21 +1,24 @@
 const Discord = require("Discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  let rMember =
+    message.guild.member(message.mentions.users.first()) ||
+    message.guild.members.get(args[0]);
+  if (!rMember) return message.reply("Could not find User.");
+  let role = message.content.split(" ").splice(2).join(" ");
+  if (!role) return message.reply("specify a Role.");
+  let gRole = message.guild.roles.find(`name`, role);
+  if (!gRole) return message.reply("Couldn't find that Role.");
 
-let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-if(!rMember) return message.reply("Could not find User.");
-let role = args.join(" ").slice(22);
-if(!role) return message.reply("specify a Role.");
-let gRole = message.guild.roles.find(`name`, role);
-if(!gRole) return message.reply("Couldn't find that Role.");
-    
-if(rMember.roles.has(gRole.id)) return message.reply("They don't have that role.");
-await(rMember.removeRole(gRole.id));
-    
-message.channel.send(`<@${rMember.id}> You no longer have ${gRole.name} role.`)
+  if (!rMember.roles.has(gRole.id))
+    return message.reply("They don't have that role.");
+  await rMember.removeRole(gRole.id);
 
-}
+  message.channel.send(
+    `<@${rMember.id}> You no longer have ${gRole.name} role.`
+  );
+};
 
 module.exports.help = {
-    name: "removerole"
-}
+  name: "removerole"
+};
