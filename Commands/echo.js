016d312a30e84c1fdb083ae4cb765b20c9289845
MeaninglessVoid <dubnersamuel@gmail.com>
2018-07-31 12:35:34 -0700

    improve echo command

--- Commands/echo.js
+++ Commands/echo.js
@@ -4,12 +4,11 @@ module.exports.run = async (bot, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES"))
     return message.reply("Missing Permission: Manage Messages");
   let botmessage = args.slice(1).join(" ");
-  console.log(args);
   message.delete().catch();
-  bot.guilds
-    .first()
-    .channels.get(message.mentions.channels.first().id)
-    .send(botmessage);
+  bot.channels
+    .get(message.mentions.channels.first().id)
+    .send(botmessage)
+    .catch(console.error);
 };
 
 module.exports.help = {
